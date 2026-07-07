a7bb3637316a04844c605611538161d383978801
taans14 <ngophuctan1407@gmail.com>
2026-07-07 20:38:29 +0700

    Set up Spring Boot

---
+++ src/test/java/com/taans/zennercore/ZennerCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taans.zennercore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZennerCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
